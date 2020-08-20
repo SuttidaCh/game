2fe94c45d0ae0705c2d822150ce174a3852cddad
SuttidaCh <suttida.20992@gmail.com>
2020-08-20 15:56:04 +0700

    enter

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app">
+    <img src="img/logo.png" width="10%" />
+    <p>
+      <br />
+    </p>
+    <HelloWorld msg="WAR OF 2020" />
+    <Game />
+    <div class="row">
+      <div class="col-4">
+        <img src class="rounded-circle" width="50%" />
+        <p>
+          <br />
+        </p>
+      </div>
+      <div class="col-4">
+        <img src class="rounded-circle" width="50%" />
+        <p>
+          <br />
+        </p>
+      </div>
+      <div class="col-4">
+        <img src class="rounded-circle" width="50%" />
+        <p>
+          <br />
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import Game from "./components/Game.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+    Game,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Game.vue
@@ -0,0 +1,73 @@
+<template>
+  <div >
+    <p>HP Player {{random(1, 3)}}
+        {{player[random(1, 3) - 1].name}}
+        </p>+รูป
+         <hr />
+        name = {{aPlayer.name}}
+        HP = {{aPlayer.hp}}
+        img = {{aPlayer.img}}
+        <hr />
+    <p>HP Monster {{random(1,3)}}
+        {{monster[random(1,3)-1].name}}</p>+รูป
+
+        <hr />
+        name = {{aMonster.name}}
+        HP = {{aMonster.hp}}
+        img = {{aMonster.img}}
+        <hr />
+
+     <button @click ="start()" class="btn btn-dark ml-2" >Start </button>
+    <button  @click ="attack()" class="btn btn-dark ml-2" >Attack</button>
+    <button  @click ="SpecialAttack()" class="btn btn-dark ml-2" >Special Attack</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+        aPlayer: { name: "" , hp: 0, imag: ''},
+        // ชื่อ player[1].name  randomเอา
+      player: [
+        { name: "Anakin", hp: 90, imag: 'a' },
+        { name: "Iron Man", hp: 150 ,imag: 'b'},
+        { name: "Doraemon", hp: 200 ,imag: 'c' },
+      ],
+       aMonster: { name: "" , hp: 0, imag: ''},
+      monster: [
+        { name: "Thanons", hp: 300 ,imag: '' },
+        { name: "Darth vader", hp: 20 ,imag: 'y' },
+        { name: "Cell", hp: 20 ,imag: 'z' },
+      ],
+    };
+  },
+  methods: {
+    random: function (min, max) {
+      return Math.max(Math.floor(Math.random() * max) + 1, min);
+    },
+
+    start:function(){
+        //1.random player 1-3  ชื่อ hp รูป 
+        this.aPlayer = this.player[this.random(1,3)-1 ]
+        this.aMonster = this.monster[this.random(1,3)-1 ]
+        //
+        //
+    },
+    attack:function(){
+        //1. player  >>> monster
+        
+        //2. player  <<< monster
+        //3. player.HP <<<=0 =>>> ?
+        //4. monster.HP <<<=0 =>>> ?
+        //5. 
+    },
+  },
+};
+</script>
+
+<style >
+div{
+     color: rgb(255, 255, 255);
+}
+</style>
